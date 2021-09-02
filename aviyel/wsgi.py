483b4b53f161f6afbe43e9dbe2450e15987a99d3
Tosin Olawore <25176099+tosinolawore@users.noreply.github.com>
2021-09-02 17:15:17 +0100

    Initial Commit

---
+++ aviyel/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for aviyel project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aviyel.settings')
+
+application = get_wsgi_application()
