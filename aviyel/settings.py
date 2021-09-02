6540782826eabd4776c9333240a630326d2b5729
Tosin Olawore <25176099+tosinolawore@users.noreply.github.com>
2021-09-03 00:26:29 +0100

    Creates CustomUser model to allow both username and email definition

--- aviyel/settings.py
+++ aviyel/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'aldjemy',
     'account.apps.AccountConfig',
 ]
 
@@ -130,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.CustomUser'
